69d5ebe53ee2d9e306952df3aa958ba1912b81f3
Agegnehu Shibiru <290303273+agegnehu2@users.noreply.github.com>
2026-08-28 03:18:10 +0300

    Add CORS middleware to server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 const pool = require("./db");
 
 const app = express();
